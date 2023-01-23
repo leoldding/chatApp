66415bd4f31042156a48ff61ea65b542df628171
leoldding <leo.ld.ding@gmail.com>
2023-01-23 11:35:29 -0500

    added base code

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/leoldding/chatApp
+
+go 1.19
---
+++ backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/ping", ping)
+
+	http.ListenAndServe(":8080", nil)
+	return
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	return
+}
